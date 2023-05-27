f5ae83cd68cc6815c15ef2ab7a29346f0231b1f0
jnrchry <jnrchry@gmail.com>
2023-05-27 13:34:12 +0000

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,55 @@
+import { useState, useEffect } from "react";
 import "./contact.scss";
+import AnimateLetters from "../AnimateLetters/AnimateLetters";
+import Loader from "react-loaders";
 
 const Contact = () => {
+  const [letterClasss, setLetterClass] = useState("text-animate");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <>
+      {loading ? (
+        <Loader type="ball-scale-multiple" />
+      ) : (
+        <div className="container contact-page">
+          <div className="text-zone">
+            <h1>
+              <AnimateLetters
+                letterClass={letterClasss}
+                stringArray={[
+                  "C",
+                  "o",
+                  "n",
+                  "t",
+                  "a",
+                  "c",
+                  "t",
+                  "  ",
+                  "m",
+                  "e",
+                ]}
+                index={15}
+              />
+            </h1>
+
+            <p></p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
